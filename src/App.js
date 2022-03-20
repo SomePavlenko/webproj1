468357ee76504f22038e11a89ab1a33145d10470
paa1030 <paa1030@gmaail.com>
2022-03-20 03:09:03 +0300

    second day

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react"; 
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css';
 
 function App() {
-
-    const [likes, setLikes] = useState(0);
-    const [value, setValue] = useState("ТЕКСТ В ИНПУТЕ");
+    const [posts, setPost] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
 
     return ( 
     < div className = "App" >
-        <Counter/>
+        <form>
+            <input type="text" placeholder="Название поста"/>
+            <input type="text" placeholder="Описание поста"/>
+            <button>Создать пост</button>
+        </form>
+        <PostList posts={posts} title = 'Посты по JS' />
     </div>
 
     );
