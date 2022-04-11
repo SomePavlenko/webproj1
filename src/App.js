70d7b1f5fe5b1b6ff1daf9b5eeb14560ff46527a
paa1030 <paa1030@gmaail.com>
2022-04-12 01:57:40 +0300

    beginer sort

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostList from "./components/PostList";
 import './styles/App.css';
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import PostForm from "./components/PostForm";
 
 function App() {
@@ -13,6 +14,20 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'Description'},
     ])
 
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    function getSortedPost(){
+        console.log('Отработала функция соритровки постов')
+        if(selectedSort){
+            return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        else
+        return posts
+    }   
+
+    const sortedPost = getSortedPost()
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
@@ -21,12 +36,37 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
     
+    const sortPost = (sort) => {
+        setSelectedSort(sort);
+    }
+
     return ( 
     < div className = "App" >
         <PostForm create = {createPost}/>
+        <hr style={{margin: '15px 0'}} />
+        <div> 
+            <MyInput
+                value = {searchQuery}
+                onChange = {e => setSearchQuery(e.target.value)}
+                placeholder="Поиск..."
+            />
+            <MySelect
+            value = { selectedSort }
+            onChange = {sortPost}
+            defaultValue="Сортировка"
+            options = {[
+                { value: 'title', name: 'По названию' },
+                { value: 'body', name: 'По описанию' },
+            ]}
+            />
+        </div>
         {posts.length
-        ?<PostList remove = {removePost} posts={posts} title = 'Посты по JS' />
-        :<h1 style= {{textAlign : "center"}}>Постов нет епта !!!</h1>
+            ?
+            <PostList remove = {removePost} posts={sortedPost} title = 'Посты по JS' />
+            :
+            <h1 style= {{textAlign : "center"}}>
+                Постов нет епта !!!
+            </h1>
         }
     </div>
 
