f5efcd034df87b12a12ac6bb2ec85754227bea6a
alpavlenko <paa1030@gmail.com>
2022-06-11 17:38:17 +0300

    02.08.00 pagination

--- src/App.js
+++ src/App.js
@@ -13,21 +13,29 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({ sort: '', query: ''})
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+    let pagesArray = getPagesArray(totalPages)
+    
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+        const response = await PostService.getAll(limit,page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit));
     })
    
     useEffect(() => {
         fetchPosts()
-    },[ ])
-
+    },[page])    
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -38,6 +46,9 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
     
+    const changePage = (page) => {
+        setPage(page)
+    }
 
     return ( 
     < div className = "App" >
@@ -66,6 +77,17 @@ function App() {
             title = 'Посты по JS'
             />
         }
+        <div className="page__wrapper">      
+            {pagesArray.map(p => 
+                <span 
+                    className={page === p ? 'page page__current' : 'page'}
+                    key = {p}
+                    onClick = {() => changePage(p)}
+                >
+                {p}
+                </span>
+            )}
+        </div>
     </div>
 
     );
