75a42ca8ecc3662a619e1b6d871f31e339b90c19
paa1030 <paa1030@gmaail.com>
2022-06-14 00:09:31 +0300

    launch router

--- src/App.js
+++ src/App.js
@@ -1,96 +1,16 @@
-import React, { useEffect, useMemo, useRef, useState } from "react"; 
-import PostItem from "./components/PostItem";
-import PostList from "./components/PostList";
-import './styles/App.css';
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import MySelect from "./components/UI/select/MySelect";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { usePosts } from "./hooks/usePosts";
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import { useFetching } from "./hooks/useFetching";
-import { getPageCount, getPagesArray } from "./utils/pages";
+import React from "react"; 
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+// import './styles/App.css';
 
 function App() {
-    const [posts, setPosts] = useState([])
-    const [filter, setFilter] = useState({ sort: '', query: ''})
-    const [modal, setModal] = useState(false);
-    const [totalPages, setTotalPages] = useState(0);
-    const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(1);
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-    let pagesArray = getPagesArray(totalPages)
-    
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostService.getAll(limit,page)
-        setPosts(response.data)
-        const totalCount = response.headers['x-total-count'];
-        setTotalPages(getPageCount(totalCount, limit));
-    })
-   
-    useEffect(() => {
-        fetchPosts()
-    },[page])    
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-    
-    const changePage = (page) => {
-        setPage(page)
-    }
-
-    return ( 
-    < div className = "App" >
-
-        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-            Создать пост 
-        </MyButton>
-        <button onClick={fetchPosts}> GET POSTS</button>
-        <MyModal visible={modal} setVisible={setModal} >
-            <PostForm create = {createPost}/>
-        </MyModal>
-        <hr style={{margin: '15px 0'}} />
-        <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-        />
-        {postError &&
-            <h1>Произошла ошибка ${postError}</h1>
-
-        }
-        {isPostsLoading
-            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-            : <PostList  
-            remove = {removePost}
-            posts={sortedAndSearchedPosts} 
-            title = 'Посты по JS'
-            />
-        }
-        <div className="page__wrapper">      
-            {pagesArray.map(p => 
-                <span 
-                    className={page === p ? 'page page__current' : 'page'}
-                    key = {p}
-                    onClick = {() => changePage(p)}
-                >
-                {p}
-                </span>
-            )}
-        </div>
-    </div>
-
-    );
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/about" element={<About/>} />
+            </Routes>
+  </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1 style={{fontSize:20, color:'black'}}>
+           СТРАНИЧКА ОБО МНЕ  
+        </h1>
+    );
+};
+
+export default About;
