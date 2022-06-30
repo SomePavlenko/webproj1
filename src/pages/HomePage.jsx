975c67555477a9fe2c616039254aaf848b517822
alPavlenko <paa1030@gmail.com>
2022-07-01 00:00:01 +0300

    add mui and start learning

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import {
+  Avatar,
+  Box,
+  Card,
+  Button,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { width } from "@mui/system";
+import React from "react";
 
 const HomePage = () => {
-    return (
-        <div>
-            HOME PAGE
-        </div>
-    );
+  return (
+ <Container>
+    <Card>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      />
+      </Card>
+
+  
+        <Paper elevation={3}>
+          <Typography variant="h4"
+          sx = {{
+          backgroundColor: "primary.dark",
+            
+          }}>
+            Hello, i am Alexandr Softwear developer from Taganrog
+            <Avatar></Avatar>
+          </Typography>
+        </Paper>
+     
+      </Container>
+  );
 };
 
 export default HomePage;
